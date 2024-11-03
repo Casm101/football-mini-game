ae7d93f7db7d701477acfd0fdc91b8eed595fc54
Christian Smith Mantas <christian.smith-mantas@leovegas.com>
2024-11-04 00:43:24 +0100

    Fix: sessionStorage issue with SSR.

--- src/hooks/useSessionStorage.ts
+++ src/hooks/useSessionStorage.ts
@@ -1,17 +1,28 @@
 import { useCallback } from 'react';
 
 export const useSessionStorage = () => {
-  const setItem = useCallback((key: string, value: string) => {
-    sessionStorage.setItem(key, value);
-  }, []);
+  const isNotSSR = typeof window !== 'undefined';
 
-  const getItem = useCallback((key: string) => {
-    return sessionStorage.getItem(key) || null;
-  }, []);
+  const setItem = useCallback(
+    (key: string, value: string) => {
+      if (isNotSSR) sessionStorage.setItem(key, value);
+    },
+    [isNotSSR]
+  );
 
-  const removeItem = useCallback((key: string) => {
-    sessionStorage.removeItem(key);
-  }, []);
+  const getItem = useCallback(
+    (key: string) => {
+      if (isNotSSR) return sessionStorage.getItem(key) || null;
+    },
+    [isNotSSR]
+  );
+
+  const removeItem = useCallback(
+    (key: string) => {
+      if (isNotSSR) sessionStorage.removeItem(key);
+    },
+    [isNotSSR]
+  );
 
   return { setItem, getItem, removeItem };
 };
